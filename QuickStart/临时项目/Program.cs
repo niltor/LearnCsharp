e7f939001310c23ed1b6cb5da15f46891aa4a6ef
zpty still <zpty@outlook.com>
2017-12-27 19:19:53 +0800

    linq示例

--- QuickStart/临时项目/Program.cs
+++ QuickStart/临时项目/Program.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace 临时项目
@@ -8,49 +11,115 @@ namespace 临时项目
     {
         static void Main(string[] args)
         {
-            //创建文件，设置编码保存
-            string content = "编码";
-            string path = @"e:\output.txt";
-            // 更多的编码支持，需要先注册
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            #region 简单数组示例
+            var numbers = new int[] { 1, 31, 22, 4, 229, 84, 8, 23 };
+            Console.WriteLine("原数组：");
+            WriteArray(numbers);
 
-            //定义编码
-            var utf8 = Encoding.UTF8;
-            var gbk = Encoding.GetEncoding("GBK");
+            // 取最大值 
+            int max = numbers.Max();
+            Console.WriteLine("最大值为:" + max);
 
-            Console.OutputEncoding = utf8;
-            //以GBK编码保存文件
-            File.WriteAllText(path, content, gbk);
-            //读取文件内容
-            string readContent = File.ReadAllText(path, gbk);
+            // 取第三大的值
+            int thirdMax = numbers.OrderByDescending(m => m)
+                .Skip(2)
+                .First();
+            Console.WriteLine("第三大的数是" + thirdMax);
 
-            // 不同编码的bytes输出
-            // 1 源字符转字节
-            var gbkBytes = gbk.GetBytes(readContent);
-            Console.WriteLine("源文件字节");
-            Print(gbkBytes);// 177 224 194 235
-            // 2 编码转换，转换字节到新编码形式
-            var utf8Bytes = Encoding.Convert(gbk, utf8, gbkBytes);
-            // 3 将新字节还原到目标编码
-            var utfContent = Encoding.UTF8.GetString(utf8Bytes);
-            Console.WriteLine("utf8的字节:");
-            Print(Encoding.UTF8.GetBytes(utfContent));// 231 188 150 231 160 129
+            // 取总和
+            int sum = numbers.Sum();
+            Console.WriteLine("总和为:" + sum);
 
-            // 直接读字节
-            var binary = File.ReadAllBytes(path);
-            Print(binary);
+            // 条件筛选，取所有奇数，并按从小到大排列
+            var result = numbers.Where(n => n % 2 == 1)
+                .OrderBy(m => m)
+                .ToArray();
+
+            Console.WriteLine("处理后的结果为:");
+            WriteArray(result);
+
+            #endregion
+
+            #region 对象类型示例
+            // 初始化数据
+            var students = InitData();
+
+            // 找出总分最高的学生
+            var student = students.Select(s =>
+            {
+                return new
+                {
+                    Sum = s.Score.Chinese + s.Score.English + s.Score.Math,
+                    s.Name
+                };
+            }).OrderByDescending(s => s.Sum)
+            .First();
+            Console.WriteLine("总分最高的学生是:" + student.Name + ";总分为:" + student.Sum);
+
+            // 找出平均分最高的班级
+            var re = students.GroupBy(s => s.Class)
+                .Select(s =>
+                {
+                    return new
+                    {
+                        Average = s.Sum(m => m.Score.Chinese + m.Score.English + m.Score.Math) / s.Count(),
+                        Class = s.Key
+                    };
+                })
+                .OrderByDescending(m => m.Average);
+
+            Console.WriteLine("最高班级:" + re.First().Class + ";平均分:" + re.First().Average);
+            //单科成绩最高的人
+            #endregion
+
         }
 
-        /// <summary>
-        /// 输出字节形式
-        /// </summary>
-        /// <param name="bytes"></param>
-        static void Print(byte[] bytes)
+
+        static List<Student> InitData()
         {
-            foreach (var item in bytes)
+            string[] names = { "张三", "王五", "李四", "赵二", "丁一", "李明", "韩梅", "梦琪", "忆柳", "之桃", "慕青", "问兰", "尔岚", "元香", "初夏" };
+            var random = new Random();
+
+            var students = new List<Student>();
+            // 创建学生对象
+            foreach (var name in names)
             {
-                Console.Write(item);
-                Console.Write(" ");
+                var student = new Student
+                {
+                    Name = name,
+                    Class = random.Next(1, 4) + "班"
+                };
+                students.Add(student);
+            }
+            // 模拟学生考试成绩
+            foreach (var student in students)
+            {
+                var score = new Score
+                {
+                    Chinese = random.Next(60, 101),
+                    English = random.Next(60, 101),
+                    Math = random.Next(60, 101)
+                };
+                student.Score = score;
+
+                Console.WriteLine($"{student.Class}[{student.Name}]的成绩为:" +
+                    $"数学:{score.Math};语文:{score.Chinese};英语:{score.English}" +
+                    $"总分为:{score.Math + score.Chinese + score.English}");
+            }
+
+            return students;
+        }
+
+        private static object List<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        static void WriteArray(IEnumerable obj)
+        {
+            foreach (var item in obj)
+            {
+                Console.Write(item.ToString() + " ");
             }
             Console.WriteLine();
         }
---
+++ QuickStart/临时项目/Student.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 临时项目
+{
+    /// <summary>
+    /// 学生类
+    /// </summary>
+    class Student
+    {
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 班级
+        /// </summary>
+        public string Class { get; set; }
+
+        /// <summary>
+        /// 成绩
+        /// </summary>
+        public Score Score { get; set; }
+    }
+}
