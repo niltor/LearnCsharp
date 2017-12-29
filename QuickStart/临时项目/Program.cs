f170adce039904c02aae6c75ce52d61e94a92659
NilTor <chenxuesong@cissdata.com>
2017-12-29 17:23:06 +0800

    暂存

--- QuickStart/临时项目/Program.cs
+++ QuickStart/临时项目/Program.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 
 namespace 临时项目
@@ -11,117 +13,35 @@ namespace 临时项目
     {
         static void Main(string[] args)
         {
-            #region 简单数组示例
-            var numbers = new int[] { 1, 31, 22, 4, 229, 84, 8, 23 };
-            Console.WriteLine("原数组：");
-            WriteArray(numbers);
-
-            // 取最大值 
-            int max = numbers.Max();
-            Console.WriteLine("最大值为:" + max);
-
-            // 取第三大的值
-            int thirdMax = numbers.OrderByDescending(m => m)
-                .Skip(2)
-                .First();
-            Console.WriteLine("第三大的数是" + thirdMax);
-
-            // 取总和
-            int sum = numbers.Sum();
-            Console.WriteLine("总和为:" + sum);
-
-            // 条件筛选，取所有奇数，并按从小到大排列
-            var result = numbers.Where(n => n % 2 == 1)
-                .OrderBy(m => m)
-                .ToArray();
-
-            Console.WriteLine("处理后的结果为:");
-            WriteArray(result);
-
-            #endregion
-
-            #region 对象类型示例
-            // 初始化数据
-            var students = InitData();
-
-            // 找出总分最高的学生
-            var student = students.Select(s =>
+            // webclient 的简单使用
+            using (var wc = new WebClient())
             {
-                return new
-                {
-                    Sum = s.Score.Chinese + s.Score.English + s.Score.Math,
-                    s.Name
-                };
-            }).OrderByDescending(s => s.Sum)
-            .First();
-            Console.WriteLine("总分最高的学生是:" + student.Name + ";总分为:" + student.Sum);
-
-            // 找出平均分最高的班级
-            var re = students.GroupBy(s => s.Class)
-                .Select(s =>
-                {
-                    return new
-                    {
-                        Average = s.Sum(m => m.Score.Chinese + m.Score.English + m.Score.Math) / s.Count(),
-                        Class = s.Key
-                    };
-                })
-                .OrderByDescending(m => m.Average);
-
-            Console.WriteLine("最高班级:" + re.First().Class + ";平均分:" + re.First().Average);
-            //单科成绩最高的人
-            #endregion
-
-        }
-
-
-        static List<Student> InitData()
-        {
-            string[] names = { "张三", "王五", "李四", "赵二", "丁一", "李明", "韩梅", "梦琪", "忆柳", "之桃", "慕青", "问兰", "尔岚", "元香", "初夏" };
-            var random = new Random();
-
-            var students = new List<Student>();
-            // 创建学生对象
-            foreach (var name in names)
-            {
-                var student = new Student
-                {
-                    Name = name,
-                    Class = random.Next(1, 4) + "班"
-                };
-                students.Add(student);
-            }
-            // 模拟学生考试成绩
-            foreach (var student in students)
-            {
-                var score = new Score
-                {
-                    Chinese = random.Next(60, 101),
-                    English = random.Next(60, 101),
-                    Math = random.Next(60, 101)
-                };
-                student.Score = score;
-
-                Console.WriteLine($"{student.Class}[{student.Name}]的成绩为:" +
-                    $"数学:{score.Math};语文:{score.Chinese};英语:{score.English}" +
-                    $"总分为:{score.Math + score.Chinese + score.English}");
+                // 设置编码
+                wc.Encoding = Encoding.UTF8;
+                // 请求内容
+                var result = wc.DownloadString("https://www.baidu.com");
+                // 保存到文件
+                File.WriteAllText("baidu.html", result);
             }
 
-            return students;
-        }
+            // httpClient 请求
 
-        private static object List<T>()
-        {
-            throw new NotImplementedException();
-        }
-
-        static void WriteArray(IEnumerable obj)
-        {
-            foreach (var item in obj)
+            using (var hc = new HttpClient())
             {
-                Console.Write(item.ToString() + " ");
+                // get请求
+                hc.GetAsync("https://www.baidu.com");
+
+                // post请求,构造不同类型的请求内容
+                var data = new List<KeyValuePair<string, string>> {
+                    new KeyValuePair<string, string>("",""),
+
+                };
+                var content = new FormUrlEncodedContent(data);
+                hc.PostAsync("https://www.baidu.com", content);
+
             }
-            Console.WriteLine();
+
         }
+
     }
 }
